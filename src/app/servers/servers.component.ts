b134482692e4b0dd3b9c0e1ef602c3bb2b47a983
vijaykglg <vijay.gupta7@yahoo.in>
2021-01-30 19:25:37 +0530

    DataBinding: Property Binding in Angular

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,15 +4,21 @@ import { Component, OnInit } from '@angular/core';
   //selector: 'app-servers',//element selector
   //selector: '[app-servers]',//attribute selector
   selector: '.app-servers',//class selector
-  //templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  //template: `
+  //  <app-server></app-server>
+  //  <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    //function() {}; Below syntax is similar to this
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+   }
 
   ngOnInit(): void {
   }
