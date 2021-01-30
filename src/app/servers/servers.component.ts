a8c22d8ce198ec3ff748fbce7fe8408b6be555a9
vijaykglg <vijay.gupta7@yahoo.in>
2021-01-30 11:09:22 +0530

    Diffrent ways of writing selectors and using it

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: 'app-servers',//element selector
+  //selector: '[app-servers]',//attribute selector
+  selector: '.app-servers',//class selector
   //templateUrl: './servers.component.html',
   template: `
     <app-server></app-server>
