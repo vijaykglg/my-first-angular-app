c03a66a035346941053567116d30e01c15ceba36
vijaykglg <vijay.gupta7@yahoo.in>
2021-01-31 12:31:16 +0530

    Assignments on Directives: Updated with TimeStamp on array

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -43,6 +43,7 @@ export class ServersComponent implements OnInit {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length +1);
+    //this.log.push(this.log.length +1);
+    this.log.push(new Date());
   }
 }
