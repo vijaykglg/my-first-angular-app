402c21822c25f49931457d6bfa1f21a2e1c3314f
vijaykglg <vijay.gupta7@yahoo.in>
2021-01-31 11:11:40 +0530

    Using Directives: ngStyle and ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,28 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles:[`
+        .online {
+            color:white;
+        }
+    `]
 })
 export class ServerComponent{
     serverId: number = 10;
     serverStatus: string = 'offline';
 
+    constructor() {
+        this.serverStatus= Math.random() > 0.5 ? 'online' : 'offline';
+    }
     getServerStatus() {
-        //this.serverStatus = 'online';
-        return 'online';
+        return this.serverStatus;
+        //return 'online';
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
